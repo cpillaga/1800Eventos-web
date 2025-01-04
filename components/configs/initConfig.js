636d0d2ded81a64eefc2fb5c363f3630d1137c6d
CristoperToapanta <84956043+CristoperToapanta@users.noreply.github.com>
2025-01-03 19:40:25 -0500

    Cambios URL Prod
    URL A Prod

--- components/configs/initConfig.js
+++ components/configs/initConfig.js
@@ -1,9 +1,13 @@
-const ambiente = 'local';
+const ambiente = 'produccion';
 const urlAmbiente = {
     local:
     {
         host: 'http://192.168.1.23:3002',
     },
+    produccion:
+    {
+        host: 'https://certificadocacup.com/api'
+    }
 };
 
 const initConfig = {
