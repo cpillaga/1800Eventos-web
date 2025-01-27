5196c52580a057e6f7bcae4ef83b2ee7f9cad588
CarlosParedes1520 <noelita6411@hotmail.com>
2025-01-27 11:40:02 -0500

    fis: mistake login v3

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+"use client";
 import "@/node_modules/react-modal-video/css/modal-video.css";
 import "../public/assets/css/eventflow.css";
 import "swiper/css";
@@ -7,15 +8,17 @@ import "swiper/css/pagination";
 import "swiper/css/free-mode";
 import { dM_Sans, inter } from "@/lib/font";
 import useAuth from "./hooks/useauth";
+import { metadata } from "./metadata";
+
 
 
 export default function RootLayout({ children }) {
+  useAuth();
   return (
     <html lang="en" className={`${dM_Sans.variable} ${inter.variable}`}>
       <body className="body-bg-color-1">
 
           {children}
- 
       </body>
     </html>
   );
